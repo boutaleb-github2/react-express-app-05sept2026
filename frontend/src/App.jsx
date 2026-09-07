1a217060e6db4e364ef611e5c7f2d9d8f1a3fdbc
habib <boutaleb.habib.github2@gmail.com>
2026-09-07 19:51:55 -0400

    API react-express + POST + DELETE

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,10 +27,7 @@ function App() {
   const [error, setError] = useState("");
   const [search, setSearch] = useState("");
   const [selectedUser, setSelectedUser] = useState(null);
-
-  const onSearchChange = (value) =>{
-        setSearch(value)
-        } 
+  const [newUserName,setNewUserName] = useState("");
 
   useEffect(() => {
     const getUsers = async () => {
@@ -54,7 +51,6 @@ function App() {
         setLoading(false);
       }
     };
-
     getUsers();
   }, []);
 
@@ -64,18 +60,61 @@ function App() {
 
   if (error) {
     return <p>Erreur : {error}</p>;
-  }
-
-  // créer une barre de recherche + un state + un input controlé 
-  const filteredUsers = users.filter((user) => user.name.toLowerCase().includes(search.toLowerCase())) 
+  } 
+  
+  const onSearchChange = (value) =>{  // on peux facilement mettre cette modification dans le composant, mais vu le state apartient à App, alors on mets la logique dans le App
+        setSearch(value)
+        } 
 
   const onSelect = (user)=>{
     selectedUser?.id === user.id ? setSelectedUser(null): setSelectedUser(user);
   }
-  function onReset() {
-  setSearch("");
-  setSelectedUser(null); 
+
+  function onReset() {   // garder la logique pour App etant donné que le state appartint à App
+    setSearch("");
+    setSelectedUser(null); 
+  }
+
+  async function handleSubmit(e){
+    e.preventDefault();
+  try{
+  
+      const response = await fetch("http://localhost:3001/api/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+         body: JSON.stringify({name:newUserName})
+      });
+      if(!response.ok) throw new Error("erreur dans l'ajout d'un nouvel user")
+        
+        const data = await response.json();        
+        setUsers((prev)=>[...prev,data.user])
+        setNewUserName("");
+  }catch(err){
+      // a completer ......
+  }
 }
+async function onDelete(id){
+  try{  
+      const response = await fetch(`http://localhost:3001/api/users/${id}`,
+      {
+      method: "DELETE",      
+      }
+    );
+      if(!response.ok) throw new Error("erreur dans la suppression")
+        
+        const data = await response.json();        
+        setUsers((prev)=>prev.filter(user=>user.id !== data.user.id))
+       
+  }catch(err){
+      // a completer ......
+  }
+
+}
+
+// créer une barre de recherche + un state + un input controlé 
+  const filteredUsers = users.filter((user) => user.name.toLowerCase().includes(search.toLowerCase())) 
 
   return (
     <div>
@@ -85,22 +124,35 @@ function App() {
 
       {filteredUsers.length > 0 ? filteredUsers.map((user) => (
         <div key={user.id}>
-        <UserCard          
+        <div style={{display:"flex",flexDirection:"center"}}>
+          <UserCard          
           user={user}
           onSelect={onSelect}                    
         />
+        <button onClick={()=>onDelete(user.id)}>Delete</button>
+        </div>
         {selectedUser?.id === user.id && (
-        <div>
-            <p style={{ color: "blue" }}>ID : {user.id}</p>
-            <p style={{ color: "blue" }}>Nom : {user.name}</p>
-            <p style={{ color: "blue" }}>Email : {user.email}</p>
-             <button style={{ color: "red", fontWeight: "bold" }} onClick={() =>  setSelectedUser(null)}>Fermer</button>
-      </div>            
+          <div>
+              <p style={{ color: "blue" }}>ID : {user.id}</p>
+              <p style={{ color: "blue" }}>Nom : {user.name}</p>
+              <p style={{ color: "blue" }}>Email : {user.email}</p>
+              <button style={{ color: "red", fontWeight: "bold" }} onClick={() =>  setSelectedUser(null)}>Fermer</button>
+          </div>            
         )}
         </div>
-      )):<h3>Aucun utilisateur ne correspondond au critère de recherche</h3>
-      }
+        )):<h3>Aucun utilisateur ne correspondond au critère de recherche</h3>
+        }
       <h1>Nombre des utilisateurs : {filteredUsers.length}</h1>
+
+      <div>
+          <form onSubmit={(e)=>handleSubmit(e)}>
+            <label htmlFor="addUserId"> Add user :         
+                <input id="addUserId" name="addUser" value={newUserName} onChange={(e)=>setNewUserName(e.target.value)} />
+            </label>
+            <button type="submit">Ajouter</button>
+          </form>
+      </div>
+      
 
     </div>
   );
